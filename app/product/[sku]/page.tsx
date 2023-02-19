b96abcd815d87e36c98243f6c599d823ae692415
Youssef Bouhjira <b.youssef91@gmail.com>
2023-02-19 17:49:04 +0100

    feat: style product details page

--- app/product/[sku]/page.tsx
+++ app/product/[sku]/page.tsx
@@ -12,27 +12,32 @@ const ProductDetailsPage = ({
   if (!product) notFound();
 
   return (
-    <div>
-      <h2 className="text-3xl font-bold underline">{product.name}</h2>
+    <main className="flex w-full flex-wrap gap-[24px] py-20 px-10">
       <div className="flex gap-x-10 gap-y-5">
         <figure className="block w-[260px] h-[300px] relative shrink-0">
           <Image src={`/${product.path}`} alt={product.name} fill />
         </figure>
-        <main className="text-[24px]">{product.description}</main>
-      </div>
-      <div className="flex gap-x-2.5">
-        <strong className="text-3xl text-red-500">
-          {product.price.currentPrice}DH
-        </strong>
-        <strong className="text-3xl overline">
-          {product.price.referencePrice}DH
-        </strong>
-      </div>
+        <section>
+          <h2 className="text-3xl font-bold mb-5">{product.name}</h2>
 
-      <button className="p-4 text-xl font-bold bg-green-600 rounded-md text-white">
-        Order via whatsapp!
-      </button>
-    </div>
+          <main className="text-[24px] mb-10">{product.description}</main>
+
+          <div className="flex justify-between items-center ">
+            <div className="flex gap-x-2.5">
+              <strong className="text-3xl text-red-500">
+                {product.price.currentPrice}DH
+              </strong>
+              <strong className="text-3xl line-through">
+                {product.price.referencePrice}DH
+              </strong>
+            </div>
+            <button className="p-4 text-xl font-bold bg-green-600 rounded-md text-white">
+              Order via whatsapp!
+            </button>
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
