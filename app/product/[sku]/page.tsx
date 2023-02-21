a45a6f32161fbeb0d9f519be64143d044933b20f
Youssef Bouhjira <b.youssef91@gmail.com>
2023-02-22 00:04:09 +0100

    feat: improve product details page in mobile

--- app/product/[sku]/page.tsx
+++ app/product/[sku]/page.tsx
@@ -12,17 +12,19 @@ const ProductDetailsPage = ({
   if (!product) notFound();
 
   return (
-    <main className="flex w-full flex-wrap gap-[24px] py-20 px-10">
-      <div className="flex gap-x-10 gap-y-5">
-        <figure className="block w-[260px] h-[300px] relative shrink-0">
+    <main className="flex flex-col lg:flex-row w-full flex-wrap gap-[24px] px-5 py-10 lg:py-20 lg:px-10">
+      <div className="flex  flex-col lg:flex-row gap-x-10 gap-y-5">
+
+        <figure className="block w-ful pt-[100%] md:pt-0 md:w-[260px] lg:h-[300px] relative shrink-0">
           <Image src={`/${product.path}`} alt={product.name} fill />
         </figure>
+
         <section>
           <h2 className="text-3xl font-bold mb-5">{product.name}</h2>
 
           <main className="text-[24px] mb-10">{product.description}</main>
 
-          <div className="flex justify-between items-center ">
+          <div className="flex flex-col lg:flex-row justify-between items-center ">
             <div className="flex gap-x-2.5">
               <strong className="text-3xl text-red-500">
                 {product.price.currentPrice}DH
