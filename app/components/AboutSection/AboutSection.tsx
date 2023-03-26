467d5a1ad652ac58634b4b34eb91f1140e35fff3
Youssef Bouhjira <b.youssef91@gmail.com>
2023-03-26 20:45:29 +0000

    feat: change about section

--- app/components/AboutSection/AboutSection.tsx
+++ app/components/AboutSection/AboutSection.tsx
@@ -1,11 +1,61 @@
 import Section from "@/app/components/Section/Section";
+import Image from "next/image";
+import Button from "@/app/shared/Button/Button";
+import cx from "classnames";
+import { Hahmlet } from "@next/font/google";
 
+const font = Hahmlet({ subsets: ["latin"] });
 export default function AboutSection() {
   return (
-    <Section title="About" className="bg-lightBackground">
-      <video controls className="m-auto inline-block">
-        <source src="/videos/home.mp4" type="video/mp4" />
-      </video>
+    <Section title="" className="bg-lightBackground">
+      <div
+        className={`justify-center
+        flex flex-col md:flex-row
+      gap-lg items-center`}
+      >
+        <div className="flex flex-col gap-lg : string, justify-center items-center">
+          <h2
+            className={cx(
+              "text-[36px] mb-10 font-bold capitalize mb-md",
+              font.className
+            )}
+          >
+            how we bring our product to life?
+          </h2>
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+
+          <div>
+            <Button
+              href="/about"
+              className="bg-transparent border-main border-4 text-main"
+            >
+              Learn More
+            </Button>
+          </div>
+          <div className="max-w-[734px] : string,">
+            <video controls>
+              <source src="/videos/home.mp4" type="video/mp4" />
+            </video>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 md:grid-cols-1 items-center : string, w-fit">
+          <Image
+            src="/photos/image5.png"
+            alt="Natural wood"
+            width={471}
+            height={473}
+          />
+          <Image
+            src="/photos/image6.png"
+            alt="Natural wood"
+            width={471}
+            height={269}
+          />
+        </div>
+      </div>
     </Section>
   );
 }
--- app/components/Section/Section.tsx
+++ app/components/Section/Section.tsx
@@ -17,14 +17,16 @@ export default function Section(props: Props) {
     <section
       className={twMerge("text-center px-5 py-xl lg:py-2xl", props.className)}
     >
-      <h2
-        className={cx(
-          "text-[36px] mb-10 font-bold capitalize mb-md",
-          font.className
-        )}
-      >
-        {props.title}
-      </h2>
+      {props.title && (
+        <h2
+          className={cx(
+            "text-[36px] mb-10 font-bold capitalize mb-md",
+            font.className
+          )}
+        >
+          {props.title}
+        </h2>
+      )}
       {props.children}
     </section>
   );
