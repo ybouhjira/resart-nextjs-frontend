2f06e67e3a77de3a93311aae0d98fb75ca590748
Youssef Bouhjira <b.youssef91@gmail.com>
2023-04-02 15:25:36 +0000

    fix: fix css import

--- app/components/MainBanner/ProductCarousel.tsx
+++ app/components/MainBanner/ProductCarousel.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import * as styles from "./image.module.scss";
+import styles from "./image.module.scss";
 import cx from "classnames";
 interface Props {
   urls: string[];
