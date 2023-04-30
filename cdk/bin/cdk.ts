f91996b7351c1f1b8fe1cfa518503d9e05935eb8
Youssef Bouhjira <bouhjira.youssef@gmail.com>
2023-04-30 19:19:54 +0100

    remove cdk

--- cdk/bin/cdk.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-import { ResartEc2RdsStack } from "../lib/resart-ec2-rds-stack";
-import { DevS3BucketStack } from "../lib/s3-dev";
-
-const app = new cdk.App();
-new ResartEc2RdsStack(app, "resart");
-new DevS3BucketStack(app, "dev");
