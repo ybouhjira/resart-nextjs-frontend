d62ca19d01e301d292e591701fdba9549390e216
Youssef Bouhjira <bouhjira.youssef@gmail.com>
2023-05-01 20:58:41 +0100

    debug AWS_REGION

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,11 +2,13 @@ import MainBanner from "@/app/components/MainBanner/MainBanner";
 import AboutSection from "@/app/components/AboutSection/AboutSection";
 import TestimonialsSection from "@/app/components/TestimonialsSection/TestimonialsSection";
 import { ListObjectsV2Command, S3Client } from "@aws-sdk/client-s3";
-import process from "process";
 
 async function getHomeCarouselImagesFromS3(): Promise<string[]> {
-  console.log("AWS_REGION", process.env.AWS_REGION);
-  const s3 = new S3Client({ region: "eu-west-3" });
+  const region = process.env.AWS_REGION;
+  console.log("AWS_REGION", region);
+  const s3 = new S3Client({
+    region: region,
+  });
 
   const { Contents } = await s3.send(
     new ListObjectsV2Command({
