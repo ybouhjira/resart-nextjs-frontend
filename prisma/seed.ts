c389755b64e78996ffa44ee01a22478c9d32e5c4
Youssef Bouhjira <bouhjira.youssef@gmail.com>
2023-05-07 16:28:59 +0100

    remove unused files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,42 +1,38 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 import products from "../app/data/products";
 
-
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 async function main() {
+  prisma.product.deleteMany();
+  const createQueries = products.map(async (product) => {
+    return prisma.product.create({
+      data: {
+        sku: product.sku,
+        name: product.name,
+        description: product.description,
+        variations: {
+          create: {
+            stock: 100,
+            referencePrice: product.variations[0].referencePrice,
+            currentPrice: product.variations[0].currentPrice,
+            color: "red",
+            images: {
+              create: {
+                path: product.variations[0].images[0],
+              },
+            },
+          },
+        },
+      },
+    });
+  });
 
-    prisma.product.deleteMany()
-    const createQueries = products.map(async product => {
-        return prisma.product.create({
-            data: {
-                sku: product.sku,
-                name: product.name,
-                description: product.description,
+  await Promise.all(createQueries);
 
-                variations: {
-                    create: {
-                        referencePrice: product.variations[0].referencePrice,
-                        currentPrice: product.variations[0].currentPrice,
-                        color: 'red',
-                        images:{
-                            create: {
-                                path: product.variations[0].images[0]
-                            }
-                        }
-                    }
-                }
-
-            }
-        })
-    })
-
-    await Promise.all(createQueries)
-
-    await prisma.$disconnect()
+  await prisma.$disconnect();
 }
 
 main().catch((error) => {
-    console.error(error)
-})
-
+  console.error(error);
+});
